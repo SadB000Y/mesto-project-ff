617a17be60a3d96441a92f2ed78565776ba78941
Viktor Ryzhikov <viktornrok@yandex.ru>
2024-12-07 18:12:38 +0300

    Version 1.1 (Little fixes)

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const template = document.querySelector('#card-template').content;
 const placesContainer = document.querySelector('.places');
 const list = placesContainer.querySelector('.places__list');
+
+
 function createCard (element, deleteCard) {
     const templateCopy = template.cloneNode(true);
     templateCopy.querySelector('.card__title').textContent = element.name;
@@ -9,10 +11,14 @@ function createCard (element, deleteCard) {
     templateCopy.querySelector('.card__delete-button').addEventListener('click', deleteCard);
     return templateCopy;
 }
+
+
 function deleteCard(element) {
     const item = element.target.closest('.card');
     item.remove();
 }
+
+
 initialCards.forEach(function (element) {
     const card = createCard(element, deleteCard);
     list.append(card);
